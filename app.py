11c5516097a7ff9c5fd596af889a2f42e992cdcd
abhigandesri13 <abhigandesri@gmail.com>
2026-07-12 23:56:05 +0530

    Created Streamlit application

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.set_page_config(
+    page_title="FitGenie AI",
+    page_icon="🏋️",
+    layout="wide"
+)
+
+st.title("🏋️ FitGenie AI")
+st.subheader("AI-Powered Fitness & Wellness Coach")
+st.write("Developed using IBM watsonx.ai and IBM watsonx Orchestrate")
