d2e244654af05d7010af60ac90616284bb60b156
Gandesri Abhilash <abhigandesri@gmail.com>
2026-07-14 15:50:55 +0530

    Clear sensitive environment variables in config.py
    Removed hardcoded values for API_KEY, PROJECT_ID, and URL.

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("FBEFJLmmFQFBsDyoPKgOkvq1G2o10at6dhEzO5Kfnogw")
-PROJECT_ID = os.getenv("a0113ebe-fa2d-4b69-bc78-e1887f387d28")
-URL = os.getenv("https://api.au-syd.watson-orchestrate.cloud.ibm.com/instances/18708a3e-47b3-41de-8e17-73acaabbffdc")
+API_KEY = os.getenv("")
+PROJECT_ID = os.getenv("")
+URL = os.getenv("")
 
 MODEL_ID = "ibm/granite-4-h-small"
